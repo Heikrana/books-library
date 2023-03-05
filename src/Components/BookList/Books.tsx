ce178b0d27da3d751b98aeefb6dd87b0d377862c
Heikrana <49754221+Heikrana@users.noreply.github.com>
2023-03-05 21:40:57 +0530

    Fix mobile view
    Remove unnecessary elements

--- src/Components/BookList/Books.tsx
+++ src/Components/BookList/Books.tsx
@@ -51,62 +51,55 @@ function Books() {
 					visible={true}
 				/>
 			</div>
-			<div className="content">
-				{domain === "subject" && <p>{name?.toUpperCase()}</p>}
-				{queryCompleted && (
-					<div className="book-main">
-						{domain === "book" && (
-							<div>
-								<div className="book-btn">
-									<button
-										disabled={startIdx <= 0}
-										onClick={() => {
-											setStartIdx(
-												(startIdx) => startIdx - 10
-											);
-											setEndIdx((endIdx) => endIdx - 10);
-											setQueryCompleted(false);
-										}}
-									>
-										<MdSkipPrevious />
-										<span>Previous</span>
-									</button>
-									<button
-										disabled={
-											!Boolean(
-												(books.numFound &&
-													endIdx < books.numFound) ||
-													(books.work_count &&
-														endIdx <
-															books.work_count)
-											)
-										}
-										onClick={() => {
-											setStartIdx(
-												(startIdx) => startIdx + 10
-											);
-											setEndIdx((endIdx) => endIdx + 10);
-											setQueryCompleted(false);
-										}}
-									>
-										<span>Next</span>
-										<MdSkipNext />
-									</button>
-								</div>
-								<p>
-									Showing books {startIdx + 1} - {endIdx}
-								</p>
+			{queryCompleted && (
+				<div className="book-main">
+					{domain === "book" && (
+						<div>
+							<div className="book-btn">
+								<button
+									disabled={startIdx <= 0}
+									onClick={() => {
+										setStartIdx(
+											(startIdx) => startIdx - 10
+										);
+										setEndIdx((endIdx) => endIdx - 10);
+										setQueryCompleted(false);
+									}}
+								>
+									<MdSkipPrevious />
+									<span>Previous</span>
+								</button>
+								<button
+									disabled={
+										!Boolean(
+											(books.numFound &&
+												endIdx < books.numFound) ||
+												(books.work_count &&
+													endIdx < books.work_count)
+										)
+									}
+									onClick={() => {
+										setStartIdx(
+											(startIdx) => startIdx + 10
+										);
+										setEndIdx((endIdx) => endIdx + 10);
+										setQueryCompleted(false);
+									}}
+								>
+									<span>Next</span>
+									<MdSkipNext />
+								</button>
 							</div>
-						)}
-						<ul className="book-list">
-							<List
-								queryCompleted={queryCompleted}
-								books={books}
-							/>
-						</ul>
-					</div>
-				)}
-			</div>
+							<p>
+								Showing books {startIdx + 1} - {endIdx}
+							</p>
+						</div>
+					)}
+					<ul className="book-list">
+						<List queryCompleted={queryCompleted} books={books} />
+					</ul>
+				</div>
+			)}
 		</>
 	);
 }
